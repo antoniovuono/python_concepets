918ca6970e67755da99cc7d26266f20a8087d867
Antonio Vuono <antoniosvuono@icloud.com>
2025-07-29 09:16:55 -0300

    refactor: improove even odd challange

--- challange_even_odd.py
+++ challange_even_odd.py
@@ -1,7 +1,7 @@
 # criar um laço que verificar se um número é par ou impar
 
-for i in range(0,10):
-  if i % 2 == 0:
-      print(i, "é par")
-  else:
-       print(i, "é impar")
+for i in range(0,50):
+    if i % 2 == 0:
+        print(i, "é par!")
+    else:
+        print(i, "é impar!")
